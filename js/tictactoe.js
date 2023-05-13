debdabfe1818a99a92b4b1223cc7719776bd5830
t-s-w <tobby.satyarama@gmail.com>
2023-05-13 16:34:09 +0800

    Implement turn-taking mechanic

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -3,6 +3,8 @@
 // hard code the winning lines to make checking for win conditions easier
 const winningLines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 const gameBoard = document.querySelector('#gameBoard');
+const cat = String.fromCodePoint(0x1F431);
+const dog = String.fromCodePoint(0x1F436);
 
 /*----- state variables -----*/
 let gameState = {
@@ -30,11 +32,23 @@ function renderBoard() {
         row.id = 'row' + rowNo;
         row.classList.add('row');
         for (let colNo of [0, 1, 2]) {
+            let cellState = gameState.board[rowNo][colNo];
             let cellNo = rowNo * 3 + colNo;
             let cell = document.createElement('td');
             cell.id = 'cell' + cellNo;
             cell.classList.add('cell');
-            cell.innerHTML = 'cell';
+            let content = '';
+            if (cellState !== undefined) {
+                if (cellState) {
+                    content = dog;
+                } else {
+                    content = cat;
+                }
+            } else {
+                cell.addEventListener('click', function () { takeTurn(cellNo) });
+            }
+            cell.innerHTML = content;
+
             row.appendChild(cell);
         }
         board.appendChild(row);
@@ -44,5 +58,7 @@ function renderBoard() {
 }
 
 function takeTurn(cellNo) {
-    console.log('tried to select cell ' + cellNo);
+    gameState.board[Math.floor(cellNo / 3)][cellNo % 3] = gameState.currentTurn;
+    gameState.currentTurn = 1 - gameState.currentTurn;
+    render();
 }
