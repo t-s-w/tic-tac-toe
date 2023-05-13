92e810f4410f918c49eea5dac09203166df703ad
t-s-w <tobby.satyarama@gmail.com>
2023-05-13 18:51:22 +0800

    Implement switching between screens

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,7 +1,8 @@
 /*----- constants -----*/
 // Number the cells from left-right, top-bottom as 0 to 8.
 // hard code the winning lines to make checking for win conditions easier
-const winningLines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+const winningLines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+const screens = ['startUI', 'boardUI'];
 const gameBoard = document.querySelector('#gameBoard');
 const cat = String.fromCodePoint(0x1F431);
 const dog = String.fromCodePoint(0x1F436);
@@ -13,13 +14,16 @@ const statusMsgs = {
     win2: dog + " It's a tie! " + cat
 }
 const gameStatusUI = document.querySelector('#gameStatus');
+class Game {
+    constructor() {
+        this.board = [];
+        this.currentTurn = 0;
+        this.winningCells = [];
+    }
+}
 
 /*----- state variables -----*/
-let gameState = {
-    board: [, , , , , , , , ,],
-    currentTurn: 0,
-    winningCells: [],
-}
+let gameState = new Game();
 
 /*----- cached elements  -----*/
 
@@ -28,10 +32,11 @@ let gameState = {
 
 
 /*----- functions -----*/
-
+//rendering UIs
 function render() {
     renderBoard();
     renderGameStatus();
+    renderResetButton();
 }
 
 function renderBoard() {
@@ -83,6 +88,22 @@ function renderGameStatus() {
     gameStatusUI.innerHTML = msg;
 }
 
+function renderResetButton() {
+    if (gameState.winner !== undefined) {
+        document.querySelector('#resetButton').classList.remove('hidden');
+    } else {
+        document.querySelector('#resetButton').classList.add('hidden');
+    }
+}
+
+// game flow control
+
+function startGame() {
+    gameState = new Game();
+    changeScreen('boardUI');
+    render();
+}
+
 function takeTurn(cellNo) {
     gameState.board[cellNo] = gameState.currentTurn;
     for (let line of winningLines) {
@@ -101,4 +122,18 @@ function takeTurn(cellNo) {
         }
     }
     render();
+}
+
+function returnMain() {
+    changeScreen('startUI');
+    gameState = new Game();
+}
+
+// UI control
+
+function changeScreen(screen) {
+    for (let screen of screens) {
+        document.querySelector('#' + screen).classList.add('hidden');
+    }
+    document.querySelector('#' + screen).classList.remove('hidden');
 }
