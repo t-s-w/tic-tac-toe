5620d598d1cdffb3aee5be95f9f5c0c0764af8ee
t-s-w <tobby.satyarama@gmail.com>
2023-05-13 22:06:37 +0800

    Improve CPU AI to cover up to 2nd turn

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -6,6 +6,7 @@ const screens = ['startUI', 'boardUI'];
 const gameBoard = document.querySelector('#gameBoard');
 const cat = String.fromCodePoint(0x1F431);
 const dog = String.fromCodePoint(0x1F436);
+const oppositeDiagonals = { 0: 8, 2: 6, 6: 2, 8: 0 };
 const statusMsgs = {
     turn0: dog + "'s Turn!",
     turn1: cat + "'s Turn!",
@@ -31,6 +32,24 @@ class cellEnumerate {
         this[undefined] = [];
         for (let cell of line) {
             this[gameState.board[cell]].push(cell);
+        }
+    }
+}
+
+class dogCornerEdgeCounter {
+    constructor(board) {
+        this.diagonal = [];
+        this.edge = [];
+        this.centre = board[4] === 0 ? [4] : [];
+        for (let diag of [0, 2, 6, 8]) {
+            if (board[diag] === 0) {
+                this.diagonal.push(diag);
+            }
+        }
+        for (let edg of [1, 3, 5, 7]) {
+            if (board[edg] === 0) {
+                this.edge.push(edg);
+            }
         }
     }
 }
@@ -187,5 +206,44 @@ function decide(board) {
             return counter[undefined][0];
         }
     }
+    // if only one space left then take it 
+    let empty = [];
+    for (var cell in [0, 1, 2, 3, 4, 5, 6, 7, 8]) {
+        if (board[cell] === undefined) {
+            empty.push(cell);
+            if (empty.length > 1) {
+                break;
+            }
+        }
+    }
+    if (empty.length === 1) {
+        return empty[0];
+    }
+    // 2nd turn hardcoding 
+    let counter = new dogCornerEdgeCounter(board);
+    if (counter.diagonal.length + counter.edge.length + counter.centre.length === 2) {
+        if (counter.diagonal.length === 2) {
+            return 1;
+        } else if (counter.edge.length === 2) {
+            if (counter.edge.includes(1)) {
+                if (counter.edge.includes(3)) {
+                    return 0;
+                } else if (counter.edge.includes(5)) {
+                    return 2;
+                } else {
+                    return 3;
+                }
+            } else if (counter.edge.includes(3)) {
+                return counter.edge.includes(7) ? 6 : 1;
+            } else {
+                return 8;
+            }
+        } else if ((counter.edge.length === 1) && (counter.diagonal.length === 1)) {
+            return oppositeDiagonals[counter.diagonal[0]];
+        }
+        else {
+            return 2;
+        }
 
+    }
 }
